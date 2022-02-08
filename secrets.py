38a43ee7c9fcf3fd7c788067bcf37cff39ba17c9
Viktor Karlsson <viktorkarlsson@Viktors-MacBook-Air.local>
2022-02-08 10:23:03 +0100

    init com

---
+++ secrets.py
@@ -0,0 +1,7 @@
+secrets = {
+    'ssid' : 'D410',
+    'password' : 'Axel2018',
+    'openweather_token' : '9f28cd4a7698732b27499b6ff13b00ba',
+    'aio_username' : 'Primme1',
+    'aio_key' : 'aio_eupD76XXJdKFTvEuq4P7PZdZX2Pu'
+}
